54463f84d0f3a96f902b094f626ba89102e7482a
泉 温 <hunter_wq@outlook.com>
2019-12-09 16:41:10 +0800

    2019.12.9 16:41

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using M_Pig.COM;
+using M_Pig.SQLite;
 using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,6 +35,8 @@ namespace M_Pig
             comSelect.DisplayMemberPath = "Description";
             comSelect.SelectedValuePath = "ComNum";
             comSelect.SelectedIndex = 0;
+            SqliteDbContext context = new SqliteDbContext();
+            var b = context.Employees.Where()
         }
     }
 }
---
+++ SQLite/SQLite.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M_Pig.SQLite
+{
+
+    public class SqliteDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public SqliteDbContext() : base("connectionSQLite")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //清除自动生成的数据表名被复数的问题
+            modelBuilder.Conventions.Remove<System.Data.Entity.ModelConfiguration.Conventions.PluralizingTableNameConvention>();
+            //modelBuilder.Entity<Employee>().ToTable("Employee");
+        }
+       
+    }
+    [Table("Employee")]
+    public partial class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    
+}
